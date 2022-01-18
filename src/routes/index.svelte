cc648c4de8bac3a964bd73e624cebb6f79444513
niiwade <niilantewade77@gmail.com>
2022-01-18 20:12:03 +0000

    added blog

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -25,7 +25,7 @@
         </div>
 
         <div class="max-w-screen-xl mx-auto mt-20">
-            <div class="container px-6 py-16 mx-auto text-center">Our future partners</div>
+            <div class=" text-white container px-6 py-16 mx-auto text-center">Our future partners</div>
             
             <div class="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
                 <div class="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1">
