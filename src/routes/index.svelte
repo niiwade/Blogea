1af32491bb415993d8a17f6662b68618e7ed7bce
niiwade <niilantewade77@gmail.com>
2022-01-18 20:06:27 +0000

    Added a subscribe page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -52,6 +52,55 @@
     </div>
 </section>
 
+
+<div class= " bg-black relative flex flex-col-reverse px-4 py-16 mx-auto lg:block lg:flex-col lg:py-32 xl:py-48 md:px-8 sm:max-w-xl md:max-w-full">
+    <div class="z-0 flex justify-center h-full -mx-4 overflow-hidden lg:pt-24 lg:pb-16 lg:pr-8 xl:pr-0 lg:w-1/2 lg:absolute lg:justify-end lg:bottom-0 lg:left-0 lg:items-center">
+      <img src="https://zzzap.io/image/zzzap.io.logo.png" class="object-cover object-right w-full h-auto lg:w-auto lg:h-full" alt="" />
+    </div>
+    <div class="relative flex justify-end max-w-xl mx-auto xl:pr-32 lg:max-w-screen-xl">
+      <div class="mb-16 lg:pr-5 lg:max-w-lg lg:mb-0">
+        <div class="max-w-xl mb-6">
+          <div>
+          </div>
+          <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+            Get updates<br class="hidden md:block" />
+            on our new endpoints
+            <span class="inline-block text-deep-purple-accent-400">online</span>
+          </h2>
+          <p class="text-base text-white md:text-lg">
+            We will love to share good news with you \o/
+          </p>
+        </div>
+        <form>
+          <div class="flex flex-col md:flex-row">
+            <input
+              placeholder="Name"
+              required=""
+              type="text"
+              class="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none md:mr-2 focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+            />
+            <input
+              placeholder="Email"
+              required=""
+              type="text"
+              class="flex-grow w-full h-12 px-4 mb-3 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none md:mb-0 focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div class="flex items-center mt-4">
+            <button
+              type="submit"
+              class="inline-flex items-center justify-center h-12 px-6 mr-6 font-bold tracking-wide text-gray-700 transition duration-200 rounded shadow-md bg-white hover:bg-black focus:shadow-outline focus:outline-none"
+            >
+              Subscribe
+            </button>
+            <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+
 <section class="bg-black dark:bg-gray-900 lg:py-12 lg:flex lg:justify-center">
     <div class="bg-gray-900 dark:bg-gray-800 lg:mx-8 lg:flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg backdrop-blur-sm	backdrop-filter: blur(40px); ">
         <div class="lg:w-1/2">
